861720ca7a088c8f7118a7a8c7107f3c7b35d360
Hercilio Júnior <hercilio.master@gmail.com>
2016-03-11 15:22:49 -0300

    FIX: crash when try to calculate without values.

--- app/src/main/java/com/example/hercilio/herciliofinal/MainActivity.java
+++ app/src/main/java/com/example/hercilio/herciliofinal/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.hercilio.herciliofinal;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     String name;
     String tipoCombustivel;
-    double gasolinePrice = 0;
+    float gasolinePrice = 0;
     float ethanolPrice = 0;
 
     @Override
@@ -29,26 +35,49 @@ public class MainActivity extends AppCompatActivity {
         String resultText;
         String resultColor;
 
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Atenção");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
         EditText inputName = (EditText)findViewById(R.id.nameInput);
         name = inputName.getText().toString();
-
-        EditText inputGasoline = (EditText)findViewById(R.id.gasolineInput);
-        gasolinePrice = Float.parseFloat(inputGasoline.getText().toString());
-
-        EditText inputEthanol = (EditText)findViewById(R.id.ethanolInput);
-        ethanolPrice = Float.parseFloat(inputEthanol.getText().toString());
-
-        if(gasolinePrice*0.7 > ethanolPrice) {
-            tipoCombustivel = "ETANOL";
-            resultColor = "#00BFA5";
-            resultText = "A gasolina só valeria a pena se o etanol custar acima de "+String.format("%.2f", gasolinePrice*0.7);
+        if(name.length() < 1){
+            alertDialog.setMessage("Preencha o nome do posto de combustíveis antes de calcular.");
+            alertDialog.show();
         }
         else {
-            tipoCombustivel = "GASOLINA";
-            resultColor = "#FFD600";
-            resultText = "O etanol só valeria a pena por um preço abaixo de "+String.format("%.2f", gasolinePrice*0.7);
+            EditText inputGasoline = (EditText)findViewById(R.id.gasolineInput);
+            EditText inputEthanol = (EditText)findViewById(R.id.ethanolInput);
+
+            if(inputGasoline.getText().length() == 0 || inputEthanol.getText().length() == 0) {
+                alertDialog.setMessage("Você precisa preencher os preços antes de calcular.");
+                alertDialog.show();
+            }
+            else {
+                gasolinePrice = Float.parseFloat(inputGasoline.getText().toString());
+                ethanolPrice = Float.parseFloat(inputEthanol.getText().toString());
+
+                if(gasolinePrice*0.7 > ethanolPrice) {
+                    tipoCombustivel = "ETANOL";
+                    resultColor = "#00BFA5";
+                    resultText = "A gasolina só valeria a pena se o etanol custar acima de "+NumberFormat.getCurrencyInstance().format(gasolinePrice * 0.7);
+                }
+                else {
+                    tipoCombustivel = "GASOLINA";
+                    resultColor = "#FFD600";
+                    resultText = "O etanol só valeria a pena por um preço abaixo de "+NumberFormat.getCurrencyInstance().format(gasolinePrice*0.7);
+                }
+                displayResult(resultTitle, tipoCombustivel, resultColor, resultText);
+            }
         }
-        displayResult(resultTitle, tipoCombustivel, resultColor, resultText);
     }
 
     private void displayResult(String intro, String result, String color, String message) {
@@ -69,7 +98,10 @@ public class MainActivity extends AppCompatActivity {
     public void onClickWhatsApp(View view) {
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "No posto de combustíveis " + name + " vale mais a pena abastecer com " + tipoCombustivel + " hoje.\n(Gasolina: "+String.format("%.2f", gasolinePrice)+" Etanol: "+ethanolPrice+")");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "No posto: " + name + "\n" +
+                "Vale mais a pena abastecer com " + tipoCombustivel + " hoje.\n" +
+                "Gasolina: "+NumberFormat.getCurrencyInstance().format(gasolinePrice)+"\n" +
+                "Etanol: "+NumberFormat.getCurrencyInstance().format(ethanolPrice));
         sendIntent.setType("text/plain");
         startActivity(sendIntent);
     }
